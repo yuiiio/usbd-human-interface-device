ddef2f60b3ccf8a08b5776039c796fe4d8d676b6
yuiiio <atbjyk@protonmail.com>
2023-09-04 16:04:42 +0900

    joystick: change descriptor

--- src/device/joystick.rs
+++ src/device/joystick.rs
@@ -11,6 +11,7 @@ pub const JOYSTICK_DESCRIPTOR: &[u8] = &[
     0x05, 0x01, // Usage Page (Generic Desktop)         5,   1
     0x09, 0x04, // Usage (Joystick)                     9,   4
     0xa1, 0x01, // Collection (Application)             161, 1
+    
     0x09, 0x01, //   Usage Page (Pointer)               9,   1
     0xa1, 0x00, //   Collection (Physical)              161, 0
     0x09, 0x30, //     Usage (LX)                        9,   48
@@ -32,21 +33,27 @@ pub const JOYSTICK_DESCRIPTOR: &[u8] = &[
     0x95, 0x02, //     Report count (2)                 149, 2,
     0x81, 0x02, //     Input (Data, Variable, Absolute) 129, 2,
     0xc0,       //   End Collection                     192,
+
     0x05, 0x09, //   Usage Page (Button)                5,   9,
-    0x19, 0x01, //   Usage Minimum (0)                  25,  1,
-    0x29, 0x08, //   Usage Maximum (8)                  41,  8,
+    0x19, 0x01, //   Usage Minimum (1)                  25,  1,
+    0x29, 0x0c, //   Usage Maximum (12)                 41,  12,
     0x15, 0x00, //   Logical Minimum (0)                21,  0
     0x25, 0x01, //   Logical Maximum (1)                37,  1,
     0x75, 0x01, //   Report Size (1)                    117, 1,
-    0x95, 0x08, //   Report Count (8)                   149, 8
+    0x95, 0x0c, //   Report Count (12)                   149, 12,
     0x81, 0x02, //   Input (Data, Variable, Absolute)   129, 2,
-    
+
+
+    // ^ 8 + 4 bits 
+    // V 4 bits
+    // repack to 2 bytes. {8buttons}, {4buttons, HatSwitch}
+
+    /* Hat Switch */
 	0x05, 0x01,							/*   USAGE_PAGE (Generic Desktop) */
-	0x09, 0x39,							/*   USAGE (Hat switch) */
 	0x09, 0x39,							/*   USAGE (Hat switch) */
 	0x15, 0x01,							/*   LOGICAL_MINIMUM (1) */
 	0x25, 0x08,							/*   LOGICAL_MAXIMUM (8) */
-	0x95, 0x02,							/*   REPORT_COUNT (2) */
+	0x95, 0x01,							/*   REPORT_COUNT (1) */
 	0x75, 0x04,							/*   REPORT_SIZE (4) */
 	0x81, 0x02,							/*   INPUT (Data,Var,Abs) */
 
@@ -54,7 +61,7 @@ pub const JOYSTICK_DESCRIPTOR: &[u8] = &[
 ];
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Default, PackedStruct)]
-#[packed_struct(endian = "lsb", size_bytes = "8")]
+#[packed_struct(endian = "lsb", size_bytes = "8", bit_numbering = "msb0")]
 pub struct JoystickReport {
     #[packed_field]
     pub lx: u8,
@@ -69,9 +76,14 @@ pub struct JoystickReport {
     #[packed_field]
     pub rz: u8,
     #[packed_field]
-    pub buttons: u8,
+    pub buttons1: u8,
+
+    // this 4 bits field pack to one byte,
+    // so reversed array order compared pre descriptor.
     #[packed_field]
-    pub hat_switch: u8,
+    pub hat_switch: [bool; 4],
+    #[packed_field]
+    pub buttons2: [bool; 4],
 }
 
 pub struct Joystick<'a, B: UsbBus> {
